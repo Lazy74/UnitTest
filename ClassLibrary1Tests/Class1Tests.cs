f421ff6a7845176489e1debe6ed38a8b4cde6534
Kasyanenko.ID <lazy-777@yandex.ru>
2016-01-04 01:23:55 +0500

    no message

---
+++ ClassLibrary1Tests/Class1Tests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void Class1Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Class1Test1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void fnTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

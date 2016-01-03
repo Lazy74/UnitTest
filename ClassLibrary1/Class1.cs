e4743fb26c19e69954edcf1ec3cefb60f1188eab
Kasyanenko.ID <lazy-777@yandex.ru>
2016-01-04 00:58:22 +0500

    no message

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private double a;
+        private double b;
+        private double c;
+        private double D;
+        public Class1()
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            D = 0;
+        }
+
+        public Class1(double na, double nb, double nc, double nD)
+        {
+            a = na;
+            b = nb;
+            c = nc;
+            D = nD;
+        }
+
+        public double fn()
+        {
+            D = b * b - 4 * a * c;
+            return D;
+        }
+    }
+}
